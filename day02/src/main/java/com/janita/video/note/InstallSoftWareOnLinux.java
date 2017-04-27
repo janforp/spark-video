c078539ec958329ed5e8f2d5de71b31ce34706e9
Janita <zhuchenjian@huizhi-tech.com>
2017-04-27 15:07:42 +0800

    day02

--- day02/src/main/java/com/janita/video/note/InstallSoftWareOnLinux.java
+++ day02/src/main/java/com/janita/video/note/InstallSoftWareOnLinux.java
@@ -40,4 +40,48 @@ public class InstallSoftWareOnLinux {
      *
      *
      */
+
+    /**
+     * 5.avi 防火墙
+     * 1.service iptables status
+     * 详见课件01_iptables教程
+     */
+
+    /**
+     * 6.avi
+     *
+     * 1.pstree
+     * 2.shell中的变量
+     * 3.系统变量$HOME :获取用户的主目录， $PWD :当前目录 $USER :当前用户.....
+     * 4.显示当前shell中所有变量 : set
+     * 5.定义变量时=两边不要空格
+     * 6.撤销变量 unset + 变量名，但是readonly B=2此变量撤销不掉
+     * 7.export 变量 可以把变量提升为全局环境变量
+     * 8.用飘号"`"可以引用上一个命令的结果 A=$(ls-la) 或者 A=`ls-la`
+     */
+
+    /**
+     * 7.avi
+     *  shell 编程
+     *  1.$?    表示上一个命令退出的状态
+     *  2.$$
+     */
+
+    /**
+     * 8.avi，9.avi
+     *
+     * 1.sed命令  删除  sed '2d' example    删除example文件的第二行 ， sed '/test/'d example 删除example中有test的行
+     * 2.awk命令
+     * 3.cut命令  可以从一个文本文件中提取文本列 echo $PATH | cut -d ':' -f 3,5
+     * 4.sort命令 对行排序
+     * 5.wc命令
+     *
+     * 使用时资料手册去查即可
+     *
+     */
+
+
+    /**
+     * last -n 5    可以查看最近登录的5个记录
+     */
 }
