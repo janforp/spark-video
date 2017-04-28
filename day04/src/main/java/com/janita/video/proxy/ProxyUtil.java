6d04000fbcc90c1a60af755e11727f8a7cbf6718
janforp <janforp@163.com>
2017-04-28 23:51:50 +0800

    day04

---
+++ day04/src/main/java/com/janita/video/proxy/ProxyUtil.java
@@ -0,0 +1,25 @@
+package com.janita.video.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by Janita on 2017-04-28 23:47
+ */
+public class ProxyUtil {
+
+    public static <T> T getProxy(final int discountCoupon,final Class<?> interfaceClass,final Class<?> implementsClass){
+
+
+        return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[]{interfaceClass}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                Integer returnValue = (Integer)method.invoke(implementsClass.newInstance(),args);
+
+                return returnValue - discountCoupon;
+            }
+        });
+
+    }
+}
