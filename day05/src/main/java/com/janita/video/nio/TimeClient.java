d1b88a1fec1646ea1fed3b318311d45ac0191f16
janforp <janforp@163.com>
2017-04-29 18:45:05 +0800

    day05

--- day05/src/main/java/com/janita/video/nio/TimeClient.java
+++ day05/src/main/java/com/janita/video/nio/TimeClient.java
@@ -1,7 +1,22 @@
 package com.janita.video.nio;
 
-/**
- * Created by Janita on 2017-04-29 17:11
- */
 public class TimeClient {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+	int port = 8080;
+	if (args != null && args.length > 0) {
+	    try {
+		port = Integer.valueOf(args[0]);
+	    } catch (NumberFormatException e) {
+		// 采用默认值
+	    }
+	}
+	new Thread(new TimeClientHandle("127.0.0.1", port), "TimeClient-001")
+		.start();
+    }
 }
+
--- day05/src/main/java/com/janita/video/nio/TimeServer.java
+++ day05/src/main/java/com/janita/video/nio/TimeServer.java
@@ -1,13 +1,24 @@
 package com.janita.video.nio;
 
-/**
- * Created by Janita on 2017-04-29 17:11
- */
+import java.io.IOException;
+
 public class TimeServer {
 
-    public static void main(String[] args) {
-        int port = 8080;
-        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
-        new Thread(timeServer,"NIO-MultiplexerTimeServer - 001").start();
+    /**
+     * @param args
+     * @throws IOException
+     * @author blackcoder
+     */
+    public static void main(String[] args) throws IOException {
+	int port = 8080;
+	if (args != null && args.length<0) {
+	    try {
+		port = Integer.valueOf(args[0]);
+	    } catch (NumberFormatException e) {
+		// 采用默认值
+	    }
+	}
+	MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+	new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
     }
 }
