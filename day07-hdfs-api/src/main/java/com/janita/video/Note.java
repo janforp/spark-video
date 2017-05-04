1bef61403919a4680e19b2479441d96228e11d14
janforp <janforp@163.com>
2017-05-04 22:36:45 +0800

    day07

---
+++ day07-hdfs-api/src/main/java/com/janita/video/Note.java
@@ -0,0 +1,12 @@
+package com.janita.video;
+
+/**
+ * Created by Janita on 2017-05-04 22:35
+ */
+public class Note {
+
+    /**
+     * NameNode:源数据
+     * DataNode:数据
+     */
+}
---
+++ day07-hdfs-api/src/main/java/com/janita/video/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Janita on 2017-05-04 22:31
+ */
+package com.janita.video;
