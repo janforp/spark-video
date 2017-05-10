0376d79290fbc3a09d015a28f393644f41827d37
janforp <janforp@163.com>
2017-05-10 21:55:08 +0800

    day09

--- day09-yarn/src/main/java/com/janita/video/ReadMe.java
+++ day09-yarn/src/main/java/com/janita/video/ReadMe.java
@@ -1,5 +1,9 @@
 package com.janita.video;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Janita on 2017-05-09 23:11
  */
@@ -8,4 +12,20 @@ public class ReadMe {
     /**
      * day09的04视频学完
      */
+
+    public static void main(String[] args) {
+
+
+        List<User> users = new ArrayList<>();
+        User user = null;
+        for (int i=0;i<4;i++){
+            user = new User();
+            user.setMoney(new BigDecimal(i));
+            user.setUsername("xm " +i);
+
+            users.add(user);
+
+        }
+        System.out.print(users);
+    }
 }
